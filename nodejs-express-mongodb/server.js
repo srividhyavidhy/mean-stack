3749814ac5c27c66c7ddbaef1dc8e4ef5a6785b0
srividhyavidhy <srividhyav44@gmail.com>
2023-11-04 11:58:40 +0530

    mean-stack-Tutorils

--- nodejs-express-mongodb/server.js
+++ nodejs-express-mongodb/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:4200"
 };
 
 app.use(cors(corsOptions));
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
